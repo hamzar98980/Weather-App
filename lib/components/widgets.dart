ac478ee287c46ce9441503c961ca85d0139111bf
hamzar98980 <hamzar98980@gmail.com>
2023-02-20 02:12:39 +0500

    first commit

---
+++ lib/components/widgets.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:proj/screens/home.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                width: MediaQuery.of(context).size.width * 1,
+                height: MediaQuery.of(context).size.height * 0.5,
+                // child: Home(),
+              ),
+              Tabwidget()
+            ]),
+      ),
+    );
+  }
+}
+
+class Tabwidget extends StatelessWidget {
+  const Tabwidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2, // length of tabs
+      initialIndex: 0,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(5),
+            decoration: const BoxDecoration(
+              color: Color.fromARGB(255, 52, 30, 133),
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(40),
+                topLeft: Radius.circular(40),
+              ),
+            ),
+            child: const TabBar(
+              labelColor: Colors.white,
+              unselectedLabelColor: Colors.grey,
+              tabs: [
+                Tab(text: 'Hourly Forecast'),
+                Tab(text: 'Weekly Forecast'),
+              ],
+            ),
+          ),
+          Container(
+              height: MediaQuery.of(context).size.height *
+                  0.35, //height of TabBarView
+              decoration: const BoxDecoration(
+                gradient: RadialGradient(
+                  colors: [
+                    Color(0xff427BD1),
+                    Color.fromARGB(255, 52, 30, 133),
+                  ],
+                ),
+              ),
+              child: TabBarView(children: <Widget>[
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Container(
+                    child: Center(
+                      child: Row(
+                        children: [
+                          Cloudy(),
+                          Cloudynow(),
+                          Cloudy(),
+                          Cloudy(),
+                          Cloudy(),
+                          Cloudy(),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  child: const Center(
+                    child: Text('Display Tab 2',
+                        style: TextStyle(
+                            fontSize: 22, fontWeight: FontWeight.bold)),
+                  ),
+                ),
+              ]))
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../components/widgets.dart';
 import '../controller/usercontroller.dart';
 
 class Home extends StatelessWidget {
@@ -36,122 +37,16 @@ class Home extends StatelessWidget {
                 ),
                 Positioned(
                   bottom: 0,
-                  child: Column(
-                    children: [
-                      Container(
-                        height: MediaQuery.of(context).size.height * 0.4,
-                        width: MediaQuery.of(context).size.width * 1,
-                        decoration: const BoxDecoration(
-                            gradient: RadialGradient(
-                              colors: [
-                                Color(0xff427BD1),
-                                // Color(0xff7758D1),
-                                Color.fromARGB(255, 52, 30, 133),
-                              ],
-                            ),
-                            borderRadius: BorderRadius.only(
-                              topRight: Radius.circular(40),
-                              topLeft: Radius.circular(40),
-                            )),
-                        child: SingleChildScrollView(
-                          scrollDirection: Axis.horizontal,
-                          child: Row(
-                            // crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Cloudy(),
-                              Padding(
-                                padding:
-                                    const EdgeInsets.fromLTRB(15, 10, 5, 0),
-                                child: Container(
-                                  height: 165,
-                                  width: 75,
-                                  decoration: const BoxDecoration(
-                                    color: Color(0xFF48319D),
-                                    boxShadow: [
-                                      BoxShadow(
-                                        color: Color.fromARGB(255, 42, 24, 107),
-                                        blurRadius: 20.0, // soften the shadow
-                                        spreadRadius: 5.0, //extend the shadow
-                                        offset: Offset(
-                                          4.0, // Move to right 10  horizontally
-                                          4.0, // Move to bottom 10 Vertically
-                                        ),
-                                      )
-                                    ],
-                                    borderRadius: BorderRadius.only(
-                                      topRight: Radius.circular(40),
-                                      topLeft: Radius.circular(40),
-                                      bottomLeft: Radius.circular(40),
-                                      bottomRight: Radius.circular(40),
-                                    ),
-                                  ),
-                                  child: Column(
-                                    children: [
-                                      Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        children: const [
-                                          Padding(
-                                            padding: EdgeInsets.only(top: 20),
-                                            child: Text(
-                                              "Now",
-                                              style: TextStyle(
-                                                fontSize: 18,
-                                                fontWeight: FontWeight.bold,
-                                                color: Colors.white,
-                                              ),
-                                            ),
-                                          )
-                                        ],
-                                      ),
-                                      Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        children: [
-                                          Padding(
-                                            padding: EdgeInsets.only(top: 23),
-                                            child: Container(
-                                              child: Image.asset(
-                                                  "assets/images/wether.png"),
-                                            ),
-                                          )
-                                        ],
-                                      ),
-                                      Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        children: const [
-                                          Padding(
-                                            padding: EdgeInsets.only(top: 13),
-                                            child: Text(
-                                              "19°",
-                                              style: TextStyle(
-                                                  fontSize: 22,
-                                                  color: Colors.white,
-                                                  fontWeight: FontWeight.bold),
-                                            ),
-                                          )
-                                        ],
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                              Cloudy(),
-                              Cloudy(),
-                              Cloudy(),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 1,
+                    child: Tabwidget(),
                   ),
                 ),
                 Positioned(
                   top: 100,
                   left: MediaQuery.of(context).size.width * 0.4,
                   child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                    // mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Container(
                         child: Text(
@@ -210,6 +105,87 @@ class Cloudy extends StatelessWidget {
                   padding: EdgeInsets.only(top: 20),
                   child: Text(
                     "12 AM",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 23),
+                  child: Container(
+                    child: Image.asset("assets/images/wether.png"),
+                  ),
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Padding(
+                  padding: EdgeInsets.only(top: 13),
+                  child: Text(
+                    "19°",
+                    style: TextStyle(
+                        fontSize: 22,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Cloudynow extends StatelessWidget {
+  const Cloudynow({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(15, 10, 5, 0),
+      child: Container(
+        height: 165,
+        width: 75,
+        decoration: const BoxDecoration(
+          color: Color.fromARGB(255, 85, 53, 204),
+          boxShadow: [
+            BoxShadow(
+              color: Color.fromARGB(255, 42, 24, 107),
+              blurRadius: 20.0, // soften the shadow
+              spreadRadius: 5.0, //extend the shadow
+              offset: Offset(
+                4.0, // Move to right 10  horizontally
+                4.0, // Move to bottom 10 Vertically
+              ),
+            )
+          ],
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(40),
+            topLeft: Radius.circular(40),
+            bottomLeft: Radius.circular(40),
+            bottomRight: Radius.circular(40),
+          ),
+        ),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Padding(
+                  padding: EdgeInsets.only(top: 20),
+                  child: Text(
+                    "Now",
                     style: TextStyle(
                       fontSize: 18,
                       fontWeight: FontWeight.bold,
